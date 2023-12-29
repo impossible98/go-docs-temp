1312d5435e5b8ad10a95b88c3d310a96000b2992
impossible98 <christoleavenow@outlook.com>
2023-12-29 23:39:34 +0800

    fix: update rspress.config.ts

--- rspress.config.ts
+++ rspress.config.ts
@@ -1,10 +1,12 @@
+// import built-in modules
 import * as path from 'path';
+// import third-party modules
 import { defineConfig } from 'rspress/config';
 
 export default defineConfig({
   root: path.join(__dirname, 'docs'),
-  title: 'Rspress',
-  description: 'Rspack-based Static Site Generator',
+  title: 'Go',
+  description: 'About Go',
   icon: '/rspress-icon.png',
   logo: {
     light: '/rspress-light-logo.png',
@@ -23,8 +25,12 @@ export default defineConfig({
       {
         icon: 'github',
         mode: 'link',
-        content: 'https://github.com/web-infra-dev/rspress',
+        content: 'https://github.com/golang/go',
       },
     ],
+  },
+  markdown: {
+    checkDeadLinks: true,
+    highlightLanguages: ['go'],
   },
 });
